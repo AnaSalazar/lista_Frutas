8de4456fe02c022438466f9c61b3aa8abd40bf59
AnaSalazar <anastephany.salazarguzman@gmail.com>
2017-03-22 23:51:59 -0600

    modificacion de funcion agregar y adicion de boton y funcion remover

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,12 +1,17 @@
-var frutas = []; //arreglo vacio para guardar mi lista
-function agregarFruta(){
-    var fruta=document.getElementById("fruta").value; //valor traido del input
-    frutas.push(fruta);//si no pongo el push antes de lo siguiente no funciona de manera correcta
-    var longitud = frutas.length;
-    var texto = ""; //string vacio para "guardar" lo que se va a imprimir
 
-    for(var i = 0; i < longitud; i++){
-        texto += "<li>" + frutas[i] + "</li>";
-    }
-    document.getElementById("lista").innerHTML = texto;
+function agregarFruta(){
+  var listaOrdenada = document.getElementById("lista");
+  var listaFrutas = document.createElement("li");
+  var frutaIngresada = document.getElementById("fruta").value;
+
+  listaFrutas.innerHTML = frutaIngresada;
+  listaOrdenada.appendChild(listaFrutas);
+  document.getElementById("fruta").value = "";
+}
+function eliminarFruta(){
+  var listaOrdenada = document.getElementById("lista");
+  var listaFrutas = document.getElementsByTagName("li");
+  var ultimaFruta = listaFrutas.length - 1;
+  
+  listaOrdenada.removeChild(listaFrutas[ultimaFruta]);
 }
